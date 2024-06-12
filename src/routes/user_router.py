26b0d7d9485f7b5d3d914da55fa61d99eb067c0d
PetrusAriaa <petrusariacr25@gmail.com>
2024-06-12 19:18:58 +0700

    feat: friend list and friend req list API - Fix user searching process - New handler for user registration

--- src/routes/user_router.py
+++ src/routes/user_router.py
@@ -1,24 +1,20 @@
-from uuid import UUID, uuid4
+from typing import Annotated, Any
+from uuid import uuid4
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
+from sqlalchemy import text
+
+from pydantic import BaseModel
+
+from .auth_router import validate_token
 from ..db import get_db
-from ..model import UserModel
-from ..dto import UserResponse
+from ..model import Users, Friends
+from ..dto import CommonUserModel, CreateUserRequest, CommonUserResponseModel,\
+    FriendRequestsModel, FriendRequestsResponseModel
 from sqlalchemy.orm import Session
 import bcrypt
-from pydantic import BaseModel
-from datetime import datetime
-# from passlib.context import CryptContext
+from datetime import date
 
-from ..utils import validate_uuid
-
-
-# bycrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-class CreateUserRequest(BaseModel):
-    username: str
-    display_name: str
-    password: str
 
 user_router = APIRouter(tags=['User'])
 
@@ -30,21 +26,26 @@ def __generate_passw(password: str):
     return str(hashed).split("'")[1]
 
 
-@user_router.get('/{user_id}', response_model=UserResponse)
-def get_user(user_id: str, db: Session=Depends(get_db)):
-    _user_id, err = validate_uuid(user_id)
-    if err != None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=err.args[0])
-    user = db.query(UserModel).where(UserModel.id == _user_id).first()
-    res = {
-        "code": status.HTTP_200_OK,
-        "data": {
-            "id": str(user.id),
-            "username": user.username,
-            "displayName": user.display_name
-        }
-    }
-    return JSONResponse(res, status_code=status.HTTP_200_OK)
+@user_router.get('/search', response_model=CommonUserResponseModel)
+def get_user(username: str, db: Session=Depends(get_db)):
+    user_list = []
+    if not len(username) > 0:
+        res = CommonUserResponseModel(
+            code=status.HTTP_200_OK,
+            data=user_list
+        )
+        return res
+    
+    users = db.query(Users).filter(Users.username.contains(username)).limit(10).all()
+    for user in users:
+        _user = user.__dict__
+        user_list.append(CommonUserModel(**_user))
+    
+    res = CommonUserResponseModel(
+        code=status.HTTP_200_OK,
+        data=user_list
+    )
+    return res
 
 
 @user_router.post("/", status_code=status.HTTP_201_CREATED)
@@ -52,14 +53,57 @@ def create_user ( create_user_request: CreateUserRequest,
                 db : Session = Depends(get_db)):
     
     _password = __generate_passw(create_user_request.password)
-    create_user_model = UserModel(
-        id=uuid4(),
-        display_name = create_user_request.display_name,
-        username = create_user_request.username,
-        password = _password,
-        created_at = datetime.now()
-    )
+    
+    user = db.query(Users).where(Users.username == create_user_request.username).first()
+    if user is not None:
+        raise HTTPException(400, detail="Username tidak dapat digunakan. Gunakan username lain.")
+    
+    user = db.query(Users).where(Users.email == create_user_request.email).first()
+    if user is not None:
+        raise HTTPException(400, detail="Email ini sudah terdaftar. Gunakan email lain.")
+    
+    
+    try:
+        create_user_model = Users(
+            id=uuid4(),
+            username = create_user_request.username,
+            email = create_user_request.email,
+            password = _password
+        )
+        db.add(create_user_model)
+        db.commit()
+        return JSONResponse({"message" : "success"})
+    except:
+        raise HTTPException(500, detail="Internal Server Error")
 
-    db.add(create_user_model)
-    db.commit()
-    return JSONResponse({"massage" : "success"})
+
+# @user_router.patch("/", status_code=status.HTTP_202_ACCEPTED)
+# def edit_info()
+
+@user_router.get("/requests")
+def get_friends_requests(session: Annotated[dict[str, Any], Depends(validate_token)],
+                        db: Session = Depends(get_db)):
+    requests_list = []
+    requests = db.execute(
+        text(f"""
+            SELECT fr.id AS id,  u.username AS username, fr.created_at AS created_at FROM friend_requests fr
+                JOIN users u ON u.id = fr.friend_id
+                WHERE fk_user_id='{str(session['id'])}';
+        """)
+    )
+    for request in requests:
+        _request = request._asdict()
+        requests_list.append(FriendRequestsModel(**_request))
+    
+    res = FriendRequestsResponseModel(
+        code=status.HTTP_200_OK,
+        data=requests_list
+    )
+    return res
+
+
+@user_router.get("/friends")
+def get_friends_list(session: Annotated[dict[str, Any], Depends(validate_token)],
+                    db: Session = Depends(get_db)):
+    friends = db.query(Friends)
+    pass
