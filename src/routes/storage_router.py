f6fe24aaa150fac56711a3f4776493d1d36c20d3
PetrusAriaa <petrusariacr25@gmail.com>
2024-06-15 21:33:20 +0700

    feat: integrate with redis server

--- src/routes/storage_router.py
+++ src/routes/storage_router.py
@@ -1,24 +1,27 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException, APIRouter
+from fastapi import Depends, File, UploadFile, HTTPException, APIRouter
 from google.cloud import storage
 from google.auth import exceptions as google_exceptions
-import os
+import redis
+from os import getenv
 
+from .auth_router import validate_token
 
-# Set up Google Cloud Storage client
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/root/fitmotion/FitMotion-service/service-account-key.json"
 client = storage.Client()
 bucket_name = "fitmotion-imu-sensor"
 bucket = client.bucket(bucket_name)
 storage_router = APIRouter(tags=["Storage"])    
 
+r = redis.Redis(host=getenv('REDIS_SERVER'))
+
 @storage_router.post("/upload-csv/")
-async def upload_csv(file: UploadFile = File(...)): 
+async def upload_csv(file: UploadFile = File(...), session=Depends(validate_token)): 
     if not file.filename.endswith(".csv"):
         raise HTTPException(status_code=400, detail="Only .csv files are allowed.")
 
     try:
         blob = bucket.blob(file.filename)
         blob.upload_from_file(file.file, content_type="text/csv")
+        r.psetex(session['id'], 10000, f"{file.filename}")
         return {"message": f"File {file.filename} uploaded successfully."}
     except google_exceptions.GoogleAuthError as e:
         raise HTTPException(status_code=500, detail="Authentication error with Google Cloud.") from e
