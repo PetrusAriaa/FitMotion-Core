3341f82bdb3e5ab3661c8691ebc3fcb55c23c90e
PetrusAriaa <petrusariacr25@gmail.com>
2024-06-18 00:40:07 +0700

    fix: unregister redis service (deprecated)

--- src/routes/storage_router.py
+++ src/routes/storage_router.py
@@ -1,17 +1,16 @@
+from time import time_ns
 from fastapi import Depends, File, UploadFile, HTTPException, APIRouter
 from google.cloud import storage
 from google.auth import exceptions as google_exceptions
-import redis
 from os import getenv
 
 from .auth_router import validate_token
 
 client = storage.Client()
-bucket_name = "fitmotion-imu-sensor"
+bucket_name = getenv("CLOUD_BUCKET")
 bucket = client.bucket(bucket_name)
 storage_router = APIRouter(tags=["Storage"])    
 
-r = redis.Redis(host=getenv('REDIS_SERVER'))
 
 @storage_router.post("/upload-csv/")
 async def upload_csv(file: UploadFile = File(...), session=Depends(validate_token)): 
@@ -19,14 +18,11 @@ async def upload_csv(file: UploadFile = File(...), session=Depends(validate_toke
         raise HTTPException(status_code=400, detail="Only .csv files are allowed.")
 
     try:
-        blob = bucket.blob(file.filename)
+        id = session['id']
+        blob = bucket.blob(f'{id}-{int(time_ns()/1000)}')
         blob.upload_from_file(file.file, content_type="text/csv")
-        r.psetex(session['id'], 10000, f"{file.filename}")
         return {"message": f"File {file.filename} uploaded successfully."}
     except google_exceptions.GoogleAuthError as e:
         raise HTTPException(status_code=500, detail="Authentication error with Google Cloud.") from e
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-# To run the application, use the following command in the terminal:
-# uvicorn your_script_name:app --reload
